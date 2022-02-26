21b788e699f1cb02942f64013ad6ccc38909af1c
sweet-kkk <72143092+sweet-kkk@users.noreply.github.com>
2022-02-26 17:45:32 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@ $('#login-button').click(function (event) {
 	var userName=document.getElementById("userName").value;
     var pwd=document.getElementById("pwd").value;
 		//修改密码请改此处
-    if(userName=="宝" &&  pwd=="20211111"){
+    if(userName=="莫菲菲" &&  pwd=="20220225"){
 			event.preventDefault();
 			$('form').fadeOut(500);
 			$('.wrapper').addClass('form-success');
